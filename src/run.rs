b29e5011e96292b0da483a30103a9dfc8206eae3
emilHof <95590295+emilHof@users.noreply.github.com>
2022-11-07 20:20:47 -0500

    Change to string method on propositions

--- src/run.rs
+++ src/run.rs
@@ -1,36 +1,53 @@
-use std::io::BufRead;
 use prop_tune::operators::Proposition;
 use prop_tune::stream::TokenStream;
+use std::io::BufRead;
 
 use super::cli::{Args, Commands};
 use super::io::{read_file_line_by_line, write_to_file};
 
 pub fn run(args: Args) {
     let input = if args.file {
-        read_file_line_by_line(&args.input).expect("a valid file path").lines().into_iter().map(|line| {
-            TryInto::<Proposition>::try_into(
-                TryInto::<TokenStream>::try_into(
-                    line.expect("the input to be of parseable form")
-                ).expect("propositions to be of valid form")
-            ).expect("proposition to be of proper form")
-        }).collect::<Vec<Proposition>>()
+        read_file_line_by_line(&args.input)
+            .expect("a valid file path")
+            .lines()
+            .into_iter()
+            .map(|line| {
+                TryInto::<Proposition>::try_into(
+                    TryInto::<TokenStream>::try_into(
+                        line.expect("the input to be of parseable form"),
+                    )
+                    .expect("propositions to be of valid form"),
+                )
+                .expect("proposition to be of proper form")
+            })
+            .collect::<Vec<Proposition>>()
     } else {
-        vec![
-            TryInto::<Proposition>::try_into(
-                TryInto::<TokenStream>::try_into(args.input).expect("proposition to be of valid form")
-            ).expect("proposition to be of proper form")
-        ]
+        vec![TryInto::<Proposition>::try_into(
+            TryInto::<TokenStream>::try_into(args.input).expect("proposition to be of valid form"),
+        )
+        .expect("proposition to be of proper form")]
     };
 
     let output = match args.command {
-        Commands::Demorg => input.into_iter().map(|prop| prop.demorg().into()).collect::<Vec<String>>(),
-        Commands::Normal => input.into_iter().map(|prop| prop.normal().into()).collect::<Vec<String>>(),
-        Commands::Simplify => input.into_iter().map(|prop| prop.simplify().into()).collect::<Vec<String>>() , 
+        Commands::Demorg => input
+            .into_iter()
+            .map(|prop| prop.demorg().into())
+            .collect::<Vec<String>>(),
+        Commands::Normal => input
+            .into_iter()
+            .map(|prop| prop.normal().into())
+            .collect::<Vec<String>>(),
+        Commands::Simplify => input
+            .into_iter()
+            .map(|prop| prop.simplify().into())
+            .collect::<Vec<String>>(),
         Commands::Analyze => unimplemented!(),
     };
 
     if let Some(filepath) = args.output {
-        output.into_iter().for_each(|out| write_to_file(out, filepath.as_str()));
+        output
+            .into_iter()
+            .for_each(|out| write_to_file(out, filepath.as_str()));
     } else {
         output.into_iter().for_each(|out| println!("{}", out));
     }
@@ -39,7 +56,7 @@ pub fn run(args: Args) {
 #[cfg(test)]
 mod test_bin {
     use super::*;
-    use prop_tune::{stream, operators}; 
+    use prop_tune::{operators, stream};
 
     #[test]
     fn test_read_and_parse() {
@@ -55,7 +72,7 @@ mod test_bin {
             let mut prop: operators::Proposition = stream.try_into().ok().unwrap();
             prop = prop.demorg();
 
-           println!("{:?}", prop);
+            println!("{:?}", prop);
         }
     }
 }
